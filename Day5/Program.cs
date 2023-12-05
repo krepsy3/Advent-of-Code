a889efe8abcb0f95892e32536a1ecf001b9b6f01
krepsy3 <>
2023-12-05 16:32:49 +0100

    not proud...

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Day5;
 
@@ -6,33 +7,197 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Task1();
-        Task2();
+        Almanach input = ParseInput();
+
+        Task1(input);
+        Task2(input);
     }
 
-    static void Task1()
+    static void Task1(Almanach almanach)
+    {
+        long result = long.MaxValue;
+
+        foreach (var seed in almanach.Seeds)
+        {
+            var loc = SeedToLocation(almanach, seed);
+
+            if (loc < result)
+            {
+                result = loc;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static void Task2(Almanach almanach)
+    {
+        long result = long.MaxValue;
+        long totalSeeds = 0;
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        for (int i = 0; i < almanach.Seeds.Count; i += 2)
+        {
+            Console.WriteLine("Processing seed batch - from {0:### ### ### ### ###}, count {1:### ### ### ### ###}.", almanach.Seeds[i], almanach.Seeds[i + 1]);
+
+            for (long seed = almanach.Seeds[i]; seed < almanach.Seeds[i] + almanach.Seeds[i + 1]; seed++)
+            {
+                var loc = SeedToLocation(almanach, seed);
+
+                if (loc < result)
+                {
+                    result = loc;
+                }
+
+                totalSeeds++;
+
+                if (totalSeeds % 10_000_000 == 0)
+                {
+                    Console.WriteLine("Another 10 million seeds processed (total {0:### ### ### ### ###})  ->  {1:mm\\:ss\\.fff}.", totalSeeds, sw.Elapsed);
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static Almanach ParseInput()
+    {
+        return Almanach.FromLines(GetInput().GetEnumerator());
+    }
+
+    static IEnumerable<string> GetInput()
     {
         using var sr = new StreamReader("input.txt");
         string? input = null;
 
         while ((input = sr.ReadLine()) is not null)
         {
-
+            yield return input;
         }
-
-        Console.WriteLine();
     }
 
-    static void Task2()
+    static long SeedToLocation(Almanach a, long seed)
     {
-        using var sr = new StreamReader("input.txt");
-        string? input = null;
+        var loc = a.Se_So.Conv(seed);
+        loc = a.So_F.Conv(loc);
+        loc = a.F_W.Conv(loc);
+        loc = a.W_Li.Conv(loc);
+        loc = a.Li_T.Conv(loc);
+        loc = a.T_H.Conv(loc);
+        loc = a.H_Lo.Conv(loc);
+        return loc;
+    }
+}
 
-        while ((input = sr.ReadLine()) is not null)
+public record class Almanach(List<long> Seeds, Conversion Se_So, Conversion So_F, Conversion F_W, Conversion W_Li, Conversion Li_T, Conversion T_H, Conversion H_Lo)
+{
+    public static readonly Regex SeedLine = new Regex("seeds:(?<seeds>( [0-9]+)+)");
+    public static readonly Regex PrologLine = new Regex("(?<type>[a-zA-Z\\-]+) map:");
+
+    public static Almanach FromLines(IEnumerator<string> lines)
+    {
+        Match m;
+        do
         {
-
+            lines.MoveNext();
+            m = SeedLine.Match(lines.Current);
         }
 
-        Console.WriteLine();
+        while (!m.Success);
+        List<long> seeds = m.Groups["seeds"].Value
+            .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => Convert.ToInt64(s))
+            .ToList();
+
+
+        Conversion? seso = null, sof = null, fw = null, wli = null, lit = null, th = null, hlo = null;
+
+        bool goon = true;
+        while (goon)
+        {
+            do
+            {
+                goon = lines.MoveNext();
+                m = PrologLine.Match(lines.Current);
+            }
+
+            while (goon && !m.Success);
+
+            if (!goon) break;
+
+            switch (m.Groups["type"].Value)
+            {
+                case "seed-to-soil":
+                    seso = Conversion.FromLines(lines);
+                    break;
+                case "soil-to-fertilizer":
+                    sof = Conversion.FromLines(lines);
+                    break;
+                case "fertilizer-to-water":
+                    fw = Conversion.FromLines(lines);
+                    break;
+                case "water-to-light":
+                    wli = Conversion.FromLines(lines);
+                    break;
+                case "light-to-temperature":
+                    lit = Conversion.FromLines(lines);
+                    break;
+                case "temperature-to-humidity":
+                    th = Conversion.FromLines(lines);
+                    break;
+                case "humidity-to-location":
+                    hlo = Conversion.FromLines(lines);
+                    break;
+                default:
+                    goon = false;
+                    break;
+            }
+        }
+
+        return new Almanach(seeds, seso, sof, fw, wli, lit, th, hlo);
+    }
+}
+
+public class Conversion
+{
+    public List<(long source, long destination, long width)> Ranges { get; protected set; } = new List<(long source, long destination, long width)>();
+
+    public static readonly Regex LineRegex = new Regex(" *(?<dest>[0-9]+) *(?<source>[0-9]+) *(?<width>[0-9]+) *");
+
+    public static Conversion FromLines(IEnumerator<string> lines)
+    {
+        Conversion result = new Conversion();
+        lines.MoveNext();
+        var m = LineRegex.Match(lines.Current);
+
+        bool nonEnd = true;
+        while (m.Success && nonEnd)
+        {
+            result.Ranges.Add(new(
+                Convert.ToInt64(m.Groups["source"].Value),
+                Convert.ToInt64(m.Groups["dest"].Value),
+                Convert.ToInt64(m.Groups["width"].Value)
+            ));
+
+            nonEnd = lines.MoveNext();
+            m = LineRegex.Match(lines.Current);
+        }
+
+        return result;
+    }
+
+    public long Conv(long num)
+    {
+        foreach (var range in Ranges)
+        {
+            if ((num >= range.source) && (num < (range.source + range.width)))
+            {
+                return range.destination + num - range.source;
+            }
+        }
+
+        return num;
     }
 }
