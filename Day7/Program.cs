2388a997c36e7d3510b6074e53a1afb6e38702b4
krepsy3 <>
2023-12-08 01:51:45 +0100

    Ctrl + Cizí, Ctrl + Vlastní

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -13,13 +13,11 @@ public class Program
 
     static void Task1()
     {
-        Hand[] hands = ParseInput().ToArray();
+        var hands = ParseInputTask1();
         long result = 0;
 
-        Hand.CompareHands(hands[1], hands[0]);
-
-        Array.Sort(hands, Hand.CompareHands);
-        for (int i = 0; i < hands.Length; i++)
+        hands.Sort(Hand.CompareHands);
+        for (int i = 0; i < hands.Count; i++)
         {
             result += hands[i].Bid * (i + 1);
         }
@@ -29,18 +27,19 @@ public class Program
 
     static void Task2()
     {
-        var hands = ParseInput();
-        long result = 1;
+        var hands = ParseInputTask2();
+        long result = 0;
 
-        foreach (var h in hands)
+        hands.Sort(Hand.CompareHands);
+        for (int i = 0; i < hands.Count; i++)
         {
-
+            result += hands[i].Bid * (i + 1);
         }
 
         Console.WriteLine(result);
     }
 
-    static List<Hand> ParseInput()
+    static List<Hand> ParseInputTask1()
     {
         List<Hand> result = new List<Hand>();
 
@@ -49,7 +48,22 @@ public class Program
 
         while ((input = sr.ReadLine()) is not null)
         {
-            result.Add(Hand.FromString(input));
+            result.Add(Hand.FromStringTask1(input));
+        }
+
+        return result;
+    }
+
+    static List<Hand> ParseInputTask2()
+    {
+        List<Hand> result = new List<Hand>();
+
+        using var sr = new StreamReader("input.txt");
+        string? input = null;
+
+        while ((input = sr.ReadLine()) is not null)
+        {
+            result.Add(Hand.FromStringTask2(input));
         }
 
         return result;
@@ -58,7 +72,7 @@ public class Program
 
 public record class Hand(char[] Cards, int[] CardVals, int Bid, Kind Kind)
 {
-    public static Hand FromString(string s)
+    public static Hand FromStringTask1(string s)
     {
         var parts = s.Trim().Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
         char[] cards = parts[0].ToCharArray();
@@ -80,15 +94,40 @@ public record class Hand(char[] Cards, int[] CardVals, int Bid, Kind Kind)
 
         for (int i = 0; i < cards.Length; i++)
         {
-            cardVals[i] = GetCardValue(cards[i]);
+            cardVals[i] = "23456789TJQKA".IndexOf(cards[i]);
         }
 
         return new(cards, cardVals, bid, kind);
     }
 
-    public static int GetCardValue(char card)
+    public static Hand FromStringTask2(string s)
     {
-        return "23456789TJQKA".IndexOf(card);
+        var parts = s.Trim().Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        char[] cards = parts[0].ToCharArray();
+        int bid = Convert.ToInt32(parts[1]);
+        int jokers = cards.Where(c => c == 'J').Count();
+        int[] groups = jokers >= 5 ? new int[] { 0 } : cards.Where(c => c != 'J').GroupBy(c => c).Select(g => g.Count()).ToArray();
+        Array.Sort(groups, (i1, i2) => i2 - i1);
+        groups[0] += jokers;
+        Kind kind = groups switch
+        {
+            [5, ..]    => Kind.Fivetuple,
+            [4, ..]    => Kind.Fourtuple,
+            [3, 2, ..] => Kind.FullHouse,
+            [3, ..]    => Kind.Threetuple,
+            [2, 2, ..] => Kind.TwoPairs,
+            [2, ..]    => Kind.Pair,
+            _          => Kind.High
+        };
+
+        int[] cardVals = new int[cards.Length];
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cardVals[i] = "J23456789TQKA".IndexOf(cards[i]);
+        }
+
+        return new(cards, cardVals, bid, kind);
     }
 
     public static int CompareHands(Hand hand1, Hand hand2)
@@ -107,6 +146,8 @@ public record class Hand(char[] Cards, int[] CardVals, int Bid, Kind Kind)
             {
                 return diff;
             }
+
+            i++;
         }
 
         return 0;
